fdd8a29d2d82f926fa5a46ffc9a26439245f3991
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-10-16 09:21:29 +0200

    new var app

--- processi_App/Program.cs
+++ processi_App/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace processi_App
 {
@@ -10,6 +11,16 @@ namespace processi_App
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Esercitazione sulla gestione");
+            Process.Start("Notepad.exe");
+            Process.Start("Notepad.exe", @"C:\Users\studenti\Desktop\processi_App\processi_App\HelloWorld.txt");
+           Process.Start("Chrome.exe", @"https://github.com/");
+            var app = new Process();
+            app.StartInfo.FileName = @"Notepad.exe";
+            app.StartInfo.Arguments = @"C:\Users\studenti\Desktop\processi_App\processi_App\HelloWorld.txt";
+            app.Start();
+            app.PriorityClass = ProcessPriorityClass.RealTime;
+            app.WaitForExit();
         }
     }
 }
